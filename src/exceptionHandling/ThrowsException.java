7ffdf3bec2e2bfdf2bd5839c4d5dc57567ab6168
vignesh9963 <vignesh161998@gmail.com>
2022-02-07 13:20:12 +0530

    Initial commit

---
+++ src/exceptionHandling/ThrowsException.java
@@ -0,0 +1,10 @@
+package exceptionHandling;
+
+public class ThrowsException {
+
+    public static void main(String args[]) {
+        //NullPointerException
+        String str = null;
+        System.out.println(str.length());
+    }
+}
